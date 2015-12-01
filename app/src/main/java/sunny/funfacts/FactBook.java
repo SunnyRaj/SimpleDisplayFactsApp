c3cf1de6baa56b7c80abf87943ebf9e2628cf0f7
Sunny Raj <sundarcs@gwu.edu>
2015-11-30 19:36:44 -0500

    Initial commit

---
+++ app/src/main/java/sunny/funfacts/FactBook.java
@@ -0,0 +1,51 @@
+package sunny.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by sunny on 11/24/15.
+ */
+public class FactBook {
+
+    public String[] mfacts = {
+            "Ants stretch when they wake up in the morning",
+            "Ostriches can run faster than horses",
+            "Olympic gold medals are actually mostly made of silver",
+            "Banging your head against a wall burns 150 calories an hour",
+            "In the UK, it is illegal to eat mince pies on Christmas Day",
+            "Pteronophobia is the fear of being tickled by feathers",
+            "When hippos are upset, their sweat turns red",
+            "A flock of crows is known as a murder",
+            "'Facebook Addiction Disorder' is a mental disorder identified by Psychologists",
+            "The average woman uses her height in lipstick every 5 years",
+            "29th May is officially 'Put a Pillow on Your Fridge Day'",
+            "Cherophobia is the fear of fun",
+            "Human saliva has a boiling point three times that of regular water",
+            "If you lift a kangaroo’s tail off the ground it can’t hop",
+            "Hyphephilia are people who get aroused by touching fabrics",
+            "Billy goats urinate on their own heads to smell more attractive to females",
+            "The person who invented the Frisbee was cremated and made into frisbees after he died",
+            "During your lifetime, you will produce enough saliva to fill two swimming pools",
+            "An eagle can kill a young deer and fly away with it",
+            "Polar bears can eat as many as 86 penguins in a single sitting",
+            "King Henry VIII slept with a gigantic axe beside him",
+            "Bikinis and tampons invented by men",
+            "If Pinokio says “My Noes Will Grow Now”, it would cause a paradox",
+            "Heart attacks are more likely to happen on a Monday",
+            "If you consistently fart for 6 years & 9 months, enough gas is produced to create the energy of an atomic bomb",
+            "An average person’s yearly fast food intake will contain 12 pubic hairs",
+            "The top six foods that make your fart are beans, corn, bell peppers, cauliflower, cabbage and milk",
+            "There is a species of spider called the Hobo Spider"
+    };
+
+    public String getFact(){
+
+        String fact = "";
+
+        // Randomly select a fact
+        Random randomgenerator = new Random(); // Construct a new Random number generator
+        int randomNumber = randomgenerator.nextInt(mfacts.length);
+        fact = mfacts[randomNumber];
+        return fact;
+    }
+}
---
+++ app/src/main/java/sunny/funfacts/MainActivity.java
@@ -0,0 +1,87 @@
+package sunny.funfacts;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Declare our view variables and assign the views from the layout file
+        final RelativeLayout RelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                // Update the label with our dynamic fact
+                factLabel.setText(fact);
+                // Update the color dynamically
+                int color = mColorWheel.getColor();
+                RelativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+                mFactBook.mfacts[0] = "Pigs are cool";
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+        String message = "Yay! Our activity was created";
+        //Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
+        Log.d(TAG,"We are logging from the OnCreate() method");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
